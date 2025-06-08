f59b694a04ea80f5972e01e2557961489be1e8bb
Veronika <v@email.com>
2025-06-08 20:22:42 +0200

    napsani casti ukolu / metoda vyhledatSvatkyKeDni(); test metody nefunkcni

--- src/test/java/cz/czechitas/ukol06/svatky/SvatkySluzbaTest.java
+++ src/test/java/cz/czechitas/ukol06/svatky/SvatkySluzbaTest.java
@@ -1,6 +1,12 @@
 package cz.czechitas.ukol06.svatky;
 
 import org.junit.jupiter.api.Test;
+import org.w3c.dom.ls.LSOutput;
+
+import java.time.Month;
+import java.time.MonthDay;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -9,5 +15,14 @@ class SvatkySluzbaTest {
     @Test
     void vyhledatSvatkyKeDni() {
         //TODO implementovat test metody vyhledatSvatkyKeDni
+
+SeznamSvatku seznamSvatku = new SeznamSvatku();
+List<String> svatkyKeDni = seznamSvatku.getSvatky().stream().filter(svatek -> svatek.getDen().equals(MonthDay.of(01,04))).map(Svatek::getJmeno).toList();
+assertEquals("Diana",svatkyKeDni);
+
+
+
+
+       
     }
 }
