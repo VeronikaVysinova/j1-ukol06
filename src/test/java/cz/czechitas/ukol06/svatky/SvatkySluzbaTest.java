47876e53f823e80da1a0babc23ecf4c34ca06602
Veronika <v@email.com>
2025-06-18 09:27:43 +0200

    ukol 06 / upravena metoda pro test

--- src/test/java/cz/czechitas/ukol06/svatky/SvatkySluzbaTest.java
+++ src/test/java/cz/czechitas/ukol06/svatky/SvatkySluzbaTest.java
@@ -11,6 +11,7 @@ import java.nio.file.Path;
 import java.time.Month;
 import java.time.MonthDay;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -23,13 +24,28 @@ class SvatkySluzbaTest {
     void vyhledatSvatkyKeDni() throws IOException {
         //TODO implementovat test metody vyhledatSvatkyKeDni
 SvatkySluzba svatkySluzba = new SvatkySluzba();
-assertEquals(List.of("Diana"),svatkySluzba.vyhledatSvatkyKeDni(MonthDay.of(01,04)));
-assertTrue(svatkySluzba.vyhledatSvatkyKeDni(MonthDay.of(01,01)).isEmpty(),"Pro zadany den, neni v souboru nalezen zadny svatek.");
+assertEquals(List.of("Diana"),svatkySluzba
+        .vyhledatSvatkyKeDni(MonthDay.of(01,04)));
+assertTrue(svatkySluzba
+        .vyhledatSvatkyKeDni(MonthDay.of(01,01))
+        .isEmpty(),"Pro zadany den, neni v souboru nalezen zadny svatek.");
+assertTrue(svatkySluzba
+        .vyhledatSvatkyKeDni(MonthDay.of(01,01))
+        .isEmpty());
+assertEquals(Arrays.asList("Karina","Vasil"), svatkySluzba.vyhledatSvatkyKeDni(MonthDay.of(01,02)));
+
+
     }
 
     @Test
     void vyhledatSvatkyDnes () throws IOException {
         SvatkySluzba svatkySluzba = new SvatkySluzba();
         assertEquals(List.of("Antonín"), svatkySluzba.vyhledatSvatkyDnes());
+    }
+    @Test
+    void testConstructorSvatkySluzba() {
+        assertDoesNotThrow(() -> {
+            SvatkySluzba instance = new SvatkySluzba();
+        });
     }
 }
